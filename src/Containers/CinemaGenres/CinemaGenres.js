53d6b7189e825387df376b8a8c5ba2b62a1bc52a
user <tsokov2t1m5@gmail.com>
2018-12-06 17:05:31 +0200

    slider_new+naw-focused2

--- src/Containers/CinemaGenres/CinemaGenres.js
+++ src/Containers/CinemaGenres/CinemaGenres.js
@@ -1,24 +1,58 @@
 import React, {Component} from 'react';
 import './CinemaGenres.scss';
 import BilletGenre from './Components/BilletGenre'
+import Card from "./Components/Card";
+// import data from "./Components/data";
 // import imgNun from "./image/NUN.jpg";
 // import {Focusable, HorizontalList} from 'react-key-navigation';
 
+const data = {
+    genres: [
+        {label: "Ужасы", id: 1, marked: true, link: "https://i.ibb.co/X7142qG/NUN.jpg"},
+        {label: "Мультфильмы", id: 2, marked: false, link: "https://i.ibb.co/KyfDyT1/monstr.jpg"},
+        {label: "Фантастика", id: 3, marked: false, link: "https://i.ibb.co/JrC2m5d/aquaman.jpg"},
+        {label: "Боевики", id: 4, marked: false, link: "https://i.ibb.co/jhRvmRy/denzel.jpg"},
+        {label: "Документальные", id: 5, marked: false, link: "https://i.ibb.co/55RbJgm/passenger.jpg"},
+        {label: "Исторические", id: 6, marked: false, link: "https://i.ibb.co/1Z3BkHx/vasilisa.jpg"},
+        {label: "Ужасы", id: 7, marked: false, link: "https://i.ibb.co/X7142qG/NUN.jpg"},
+        {label: "Мультфильмы", id: 8, marked: false, link: "https://i.ibb.co/KyfDyT1/monstr.jpg"},
+        {label: "Фантастика", id: 9, marked: false, link: "https://i.ibb.co/JrC2m5d/aquaman.jpg"}
+    ]
+};
+
 class CinemaGenres extends Component {
-    state = {
-        genres: [
-            {label: "Ужасы", id: 1, marked: true, link: "https://i.ibb.co/X7142qG/NUN.jpg"},
-            {label: "Мультфильмы", id: 2, marked: false, link:"https://i.ibb.co/KyfDyT1/monstr.jpg" },
-            {label: "Фантастика", id: 3, marked: false, link:"https://i.ibb.co/JrC2m5d/aquaman.jpg" },
-            {label: "Боевики", id: 4, marked: false, link:"https://i.ibb.co/jhRvmRy/denzel.jpg" },
-            {label: "Документальные", id: 5, marked: false, link:"https://i.ibb.co/55RbJgm/passenger.jpg" }
-        ]
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            genres: data.genres,
+            genre: data.genres[0]
+        }
+    }
+
+    nextProperty = () => {
+        const newId = this.state.genre.id + 1;
+        this.setState({
+            genre: data.genres[newId]
+        });
+        console.log(newId)
+    };
+
+    prevProperty = () => {
+        const newId = this.state.genre.id - 1;
+        this.setState({
+            genre: data.genres[newId]
+        });
+        console.log(newId)
     };
 
     render() {
         const genres = this.state.genres.map(genre => {
             return (
-                <BilletGenre>
+                <BilletGenre
+                    key={genre.id}
+                    genre={genre}
+                    style={{'transform': `translateX(-${genre.id * (100 / genre.length)}%)`}}>
                     <img className="genres__list_picture" src={genre.link} alt={genre.label}/>
                     <p className="genres__list_text">{genre.label}</p>
                 </BilletGenre>
@@ -27,6 +61,15 @@ class CinemaGenres extends Component {
         return (
             <div className="genres">
                 <ul className="genres__list">
+
+                    <span className="prev"
+                        onClick={() => this.prevProperty()}
+                        disabled={this.state.genre.id === 0}
+                    >Prev</span>
+                    <span className="next"
+                          onClick={() => this.nextProperty()}
+                          disabled={this.state.genre.id === data.genres.length - 1}
+                    >Next</span>
                     {genres}
                 </ul>
             </div>
