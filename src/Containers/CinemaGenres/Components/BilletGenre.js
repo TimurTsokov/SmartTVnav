d716b60ffae0bfb5a1cbde30aa2e9cb1272d9b6d
TimurTsokov <tsokov2t1m5@gmail.com>
2018-12-10 18:09:33 +0200

    changes in slide.js & scss

--- src/Containers/CinemaGenres/Components/BilletGenre.js
+++ src/Containers/CinemaGenres/Components/BilletGenre.js
@@ -2,39 +2,45 @@ import React from 'react';
 import './BilletGenre.scss';
 // import { Focusable } from 'react-key-navigation';
 import Nav from 'react-navtree';
+import PropTypes from 'prop-types';
 
-// class BilletGenre extends React.Component {
-//     constructor() {
-//         super();
+// const BilletGenre = (props) => {
 //
-//         this.state = {
-//             active: false
-//         }
-//     }
-const BilletGenre = (props) => {
+//         return (
+//             <Nav defaultFocused={props.id === 1}
+//                  func={(key) => props.resolveFunc(key)}
+//                  className="genres__list_item nav">
+//                 {props.children}
+//             </Nav>
+//         );
+//
+// };
+//
+// export default BilletGenre;
 
-        return (
-            <Nav defaultFocused={props.id === 1}
-                 func={(key) => props.resolveFunc(key)}
-                 className="genres__list_item nav">
-                {props.children}
-            </Nav>
-        );
-
+const Slider = ({image, width, height}, props) => {
+    const backgroundImage = require(`./img/${image}`);
+    const styles = {
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundPosition: 'center top',
+        backgroundRepeat: 'no-repeat',
+        float: 'left',
+        width: `${width}px`,
+        height: `${height}px`
+    };
+    return (
+        <Nav efaultFocused={props.id === 1}
+            className="slide" style={styles}>
+        </Nav>
+    )
 };
-    // render() {
-    //     return (
-    //         <Nav defaultFocused={true}
-    //              onClick={() => props.inputText(props.children)}
-    //              nv-el nv-el-current={props.children === '1'}
-    //              className="genres__list_item">
-    //             {props.children}
-    //         </Nav>
-    //         //   <Focusable onFocus={() => this.setState({active: true})}
-    //         //              onBlur={() => this.setState({active: false})}>
-    //         //     <li class={'genres__list_item ' + (this.state.active ? 'focus' : '')}></li>
-    //         //   </Focusable>
-    //     );
-    // }
-
-export default BilletGenre;
+Slider.defaultProps = {
+    width: 300,
+    height: 600
+};
+Slider.propTypes = {
+    image: PropTypes.string.isRequired,
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired
+};
+export default Slider;
