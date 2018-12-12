cf4d5432027f01899b8895a6114abffc4ec2751d
SoshinaMaryana <mbazileva91@gmail.com>
2018-12-12 14:56:50 +0200

    update deviceService

--- src/modules/Device.js
+++ src/modules/Device.js
@@ -1,6 +1,4 @@
-// import './custom/webOS';
-
-
+import './custom/webOS'
 class Device {
     constructor() {
     }
@@ -33,66 +31,66 @@ class Device {
             }
         }
 
-        // function init() {
-        //     switch (device.sub_type) {
-        //         case 'DST_LG':
-        //             if (typeof window.webos === 'object' || match != null) {
-        //                 try {
-        //                     webOS.service.request('luna://com.webos.service.tv.systemproperty', {
-        //                         method: 'getSystemInfo',
-        //                         parameters: {
-        //                             'keys': ['modelName', 'sdkVersion', 'firmwareVersion']
-        //                         },
-        //                         onSuccess: function (result) {
-        //                             device.model = result.modelName + '(' + result.sdkVersion + ')';
-        //                             sdkVersion = result.sdkVersion;
-        //                             // device.firmware = result.firmwareVersion;
-        //                             try {
-        //                                 webOS.service.request('luna://com.webos.service.sm', {
-        //                                     method: 'deviceid/getIDs',
-        //                                     parameters: {
-        //                                         'idType': ['LGUDID']
-        //                                     },
-        //                                     onSuccess: function (result) {
-        //                                         device.uuid = result.idList[0].idValue;
-        //                                     },
-        //                                     onFailure: function () {
-        //                                         if (localStorage.getItem('uuid')) {
-        //                                             device.uuid = localStorage.getItem('uuid');
-        //                                         } else {
-        //                                             device.uuid = DeviceUIDGeneratorService.generateUID() + '-WebOS-' + sdkVersion.slice(0, 1);
-        //                                             localStorage.setItem('uuid', device.uuid);
-        //                                         }
-        //                                     }
-        //                                 });
-        //                             } catch (e) {
-        //                                 console.log(e)
-        //                             }
-        //                         }
-        //                     });
-        //                 } catch (e) {
-        //                 }
-        //                 try {
-        //                     webOS.service.request('luna://com.webos.settingsservice', {
-        //                         method: 'getSystemSettings',
-        //                         parameters: {
-        //                             'keys': ['localeInfo']
-        //                         },
-        //                         onSuccess: function (result) {
-        //                             onDeviceLang = (result.settings.localeInfo.locales.TV).slice(0, 2);
-        //                         }
-        //                     });
-        //
-        //                 } catch (e) {
-        //                 }
-        //             }
-        //         case 'DST_LG':
-        //             console.log('1111');
-        //             break;
-        //     }
-        // }
-        // init();
-        // console.log('webOS', webOS);
+        function init() {
+            switch (device.sub_type) {
+                case 'DST_LG':
+                    if (typeof window.webos === 'object' || match != null) {
+                        try {
+                            webOS.service.request('luna://com.webos.service.tv.systemproperty', {
+                                method: 'getSystemInfo',
+                                parameters: {
+                                    'keys': ['modelName', 'sdkVersion', 'firmwareVersion']
+                                },
+                                onSuccess: function (result) {
+                                    device.model = result.modelName + '(' + result.sdkVersion + ')';
+                                    sdkVersion = result.sdkVersion;
+                                    // device.firmware = result.firmwareVersion;
+                                    try {
+                                        webOS.service.request('luna://com.webos.service.sm', {
+                                            method: 'deviceid/getIDs',
+                                            parameters: {
+                                                'idType': ['LGUDID']
+                                            },
+                                            onSuccess: function (result) {
+                                                device.uuid = result.idList[0].idValue;
+                                            },
+                                            onFailure: function () {
+                                                if (localStorage.getItem('uuid')) {
+                                                    device.uuid = localStorage.getItem('uuid');
+                                                } else {
+                                                    device.uuid = DeviceUIDGeneratorService.generateUID() + '-WebOS-' + sdkVersion.slice(0, 1);
+                                                    localStorage.setItem('uuid', device.uuid);
+                                                }
+                                            }
+                                        });
+                                    } catch (e) {
+                                        console.log(e)
+                                    }
+                                }
+                            });
+                        } catch (e) {
+                        }
+                        try {
+                            webOS.service.request('luna://com.webos.settingsservice', {
+                                method: 'getSystemSettings',
+                                parameters: {
+                                    'keys': ['localeInfo']
+                                },
+                                onSuccess: function (result) {
+                                    onDeviceLang = (result.settings.localeInfo.locales.TV).slice(0, 2);
+                                }
+                            });
+
+                        } catch (e) {
+                        }
+                    }
+                case 'DST_LG':
+                    console.log('1111');
+                    break;
+            }
+        }
+        init();
+        console.log('webOS', webOS);
         return device;
 
     }
